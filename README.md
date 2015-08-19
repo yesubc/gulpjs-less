87dbe4d305fdb8fb2a75f02c8ab3d1d5decb1357
Sunil Shrestha <sshrestha@lftechnology.com>
2015-08-19 12:20:48 +0545

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Gulp and Bower Workshop
+===================
+
+### Getting Started
+    
+#### Install gulp globally:
+    $ npm install --global gulp
+
+#### Install gulp in your project:
+    $ npm install --save-dev gulp
+        
+#### Install server-side dependencies
+    $ npm install
+
+#### Install Gulp Less
+    npm install gulp-less
+    
+#### Run Gulp tasks
+    $ gulp
+    
+Note: You need to install `nodejs`, `npm`, & `bower` if you don't have them already.
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+;
+(function () {
+    'use strict';
+
+    var gulp        = require('gulp'),
+        wrap        = require('gulp-wrap'),
+        less        = require('gulp-less'),
+        watch       = require('gulp-watch'),
+        concat      = require('gulp-concat'),
+        notify      = require("gulp-notify"),
+        declare     = require('gulp-declare'),
+        path        = require('path');
+
+    // Source Path
+    var SOURCE_LESS = 'resources/less/**/*.less';
+
+    // Less Compilation
+    gulp.task('less', function () {
+      return gulp.src('resources/less/**/*.less')
+        .pipe(less({
+          paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('public/css'));
+    });
+
+    // Watch task
+    gulp.task('watch', function () {
+        gulp.watch(SOURCE_LESS, ['less']);
+    });
+
+    gulp.task('default', ['less']);    
+
+})();
